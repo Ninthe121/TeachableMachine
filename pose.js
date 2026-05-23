7a1ce0f02f6934b9534535292019990bc12f1022
Ninthe Gribbe <r1102090@student.thomasmore.be>
2026-05-23 16:06:57 +0200

    Fix webcam visibility

--- pose.js
+++ pose.js
@@ -1,4 +1,4 @@
-const MODEL_URL = "https://teachablemachine.withgoogle.com/models/7rnRte3ft/";
+const MODEL_URL = "./my-pose-model/";
 
 let model, webcam, ctx, maxPredictions;
 
@@ -6,9 +6,14 @@ async function initPose() {
     model = await tmPose.load(MODEL_URL + "model.json", MODEL_URL + "metadata.json");
     maxPredictions = model.getTotalClasses();
 
-    webcam = new tmPose.Webcam(400, 400, true);
+    webcam = new tmPose.Webcam(300, 225, true);
     await webcam.setup();
     await webcam.play();
+
+    const canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    canvas.width = 300;
+    canvas.height = 225;
 
     window.requestAnimationFrame(loop);
 }
@@ -36,4 +41,6 @@ async function predict() {
     if (highestScore > 0.8) {
         handlePose(detectedPose);
     }
+
+    ctx.drawImage(webcam.canvas, 0, 0);
 }
